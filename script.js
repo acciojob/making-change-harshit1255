fe481eefb0abc22d260cf1399db44e698828c6f7
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-27 02:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let obj ={}
+	let q=0, p=0,d=0,n=0;
+	obj['q'] = parseInt(c/25);
+	c = c%25;
+	obj['d'] =parseInt(c/10);
+	c=c%10;
+	obj['n']= parseInt(c/5);
+	c=c%5;
+	obj['p']= c;
+	return obj
+	
+	
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(c))); 
